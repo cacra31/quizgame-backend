740d71ac110b920975a898a560d25dbfe61e8ab5
sewon <cacra31@nis.co.kr>
2025-11-19 16:11:18 +0900

    공통 설정 수정 - 웹소켓 설정 추가

---
+++ src/main/java/com/quizgame/websocket/config/WebSocketConfig.java
@@ -0,0 +1,33 @@
+package com.quizgame.websocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+
+        // ✅ 구독(prefix): 서버 → 클라이언트로 메시지 보낼 때 사용
+        registry.enableSimpleBroker("/topic");
+
+        // ❗ 클라이언트가 메시지 보낼 때 prefix
+        // 예: 클 → 서 "/"app/chat.sendMessage"
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+
+        // 클라이언트 WebSocket 연결 URL: ws://서버주소/ws
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();  // SockJS fallback 활성화
+    }
+
+}
