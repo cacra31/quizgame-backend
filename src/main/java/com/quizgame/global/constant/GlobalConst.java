4c5e9cd2d138e5edbf4a42b0a5406a82af8b81ee
sewon <cacra31@nis.co.kr>
2025-11-26 07:58:19 +0900

    feat(global) - redis 키 상수

---
+++ src/main/java/com/quizgame/global/constant/GlobalConst.java
@@ -0,0 +1,8 @@
+package com.quizgame.global.constant;
+
+public abstract class GlobalConst {
+    public static final String ROOM_SEQ_KEY = "quiz:seq:room";
+    public static final String GAME_SEQ_KEY = "quiz:seq:game";
+    public static final String QUESTION_SEQ_KEY = "quiz:seq:question";
+    public static final String ANSWER_SEQ_KEY = "quiz:seq:answer";
+}
