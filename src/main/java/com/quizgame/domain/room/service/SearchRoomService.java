58994819d18cdc70713dd1b1b2cd235a4d294c33
sewon <cacra31@nis.co.kr>
2025-11-26 08:00:05 +0900

    feat(Room) - 방 리스트

---
+++ src/main/java/com/quizgame/domain/room/service/SearchRoomService.java
@@ -0,0 +1,42 @@
+package com.quizgame.domain.room.service;
+
+import com.quizgame.domain.category.repository.CategoryRepository;
+import com.quizgame.domain.room.api.v1.dto.RoomListResponse;
+import com.quizgame.domain.room.api.v1.dto.RoomRedisVo;
+import com.quizgame.domain.room.domain.Room;
+import com.quizgame.domain.room.redis.RoomRedisService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class SearchRoomService {
+
+    private final CategoryRepository categoryRepository;
+    private final RoomRedisService roomRedisService;
+
+    public List<RoomListResponse> execute() {
+        List<RoomListResponse> roomListResponses = new ArrayList<>();
+        categoryRepository.findAll().forEach(category -> {
+            Long waitingRoomId = roomRedisService.getWaitingRoom(category.getId());
+            if (waitingRoomId != null) {
+                RoomRedisVo waitingRoom = roomRedisService.getRoom(waitingRoomId);
+                roomListResponses.add(RoomListResponse.builder()
+                        .roomId(waitingRoom.roomId())
+                        .categoryName(category.getName())
+                        .createdAt(waitingRoom.createdAt())
+                        .build());
+            } else {
+                roomListResponses.add(RoomListResponse.builder()
+                        .categoryId(category.getId())
+                        .categoryName(category.getName())
+                        .build());
+            }
+        });
+        return roomListResponses;
+    }
+
+}
