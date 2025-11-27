9ebd220b26006ced45fbf239d3110d08377a23d4
sewon <cacra31@nis.co.kr>
2025-11-27 16:56:08 +0900

    feat(Question) - 문제 정답 dto

--- src/main/java/com/quizgame/domain/question/api/v1/dto/QuestionDto.java
+++ src/main/java/com/quizgame/domain/question/api/v1/dto/QuestionDto.java
@@ -1,9 +1,11 @@
 package com.quizgame.domain.question.api.v1.dto;
 
 import com.quizgame.domain.answer.api.v1.dto.AnswerDto;
+import lombok.Builder;
 
 import java.util.List;
 
+@Builder
 public record QuestionDto(
         Long categoryId,
         Long questionId,
