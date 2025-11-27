bd643fc35b266bc837f1f0433b8812a918bbda63
sewon <cacra31@nis.co.kr>
2025-11-27 11:45:43 +0900

    fix(auth) - 로그 추가

--- src/main/java/com/quizgame/domain/auth/api/AuthController.java
+++ src/main/java/com/quizgame/domain/auth/api/AuthController.java
@@ -10,11 +10,13 @@ import com.quizgame.global.session.SessionRedisService;
 import com.quizgame.global.session.SessionUser;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import static com.quizgame.global.util.SessionUtil.LOGIN_USER;
 
+@Slf4j
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
@@ -25,14 +27,17 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest, HttpSession session) {
+        log.info("=== /auth/login ===");
         SessionUser sessionUser = SessionUser.from(authService.login(loginRequest));
         session.setAttribute(LOGIN_USER, sessionUser);
+        log.debug("login sessionId = {}", session.getId());
         sessionRedisService.saveSession(session.getId(), sessionUser);
         return ResponseEntity.ok(new LoginResponse(sessionUser.userId(), sessionUser.name()));
     }
 
     @GetMapping("/me")
     public ResponseEntity<LoginResponse> me(HttpSession session) {
+        log.info("=== /auth/me ===");
         SessionUser loginUser = (SessionUser) session.getAttribute(LOGIN_USER);
         if (loginUser == null) throw new QuizGameException(SystemMessageCode.UNAUTHORIZED);
         return ResponseEntity.ok(new LoginResponse(loginUser.userId(), loginUser.name()));
@@ -40,6 +45,7 @@ public class AuthController {
 
     @PostMapping("/logout")
     public ResponseEntity<CommonResponse> logout(HttpSession session) {
+        log.info("=== /auth/logout ===");
         if (session != null) {
             String sessionId = session.getId();
             sessionRedisService.deleteSession(sessionId);
